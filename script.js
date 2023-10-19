df146fd2ac2408a8a9f812eae225d9b50fdb9d87
Medhi OUKILI <medhioukili@outlook.fr>
2023-10-19 09:45:23 +0200

    script

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// Sélectionnez les image par leurs ID
+// Sélectionne les image par leurs ID
 const balrog = document.getElementById("balrog");
 const gandalf = document.getElementById("gandalf");
 
-// Initialisez une variable d'état
+// Initialise une variable d'état
 let etat = "visible"; // visible par defaut
 
-// Ajoutez un gestionnaire d'événements pour le clic sur le bouton
+// Ajoutez un gestionnaire d'événements pour le clic sur gandalf
 gandalf.addEventListener("click", function() {
     if (etat === "visible") {
       document.getElementById("balrog").style.display="none"; // Fait disparaitre le balrog
-      etat = "invisible"; // Changez l'état
+      etat = "invisible"; // Change l'état
     } else {
-      document.getElementById("balrog").style.display="block"; // Changez le texte de retour
-      etat = "visible"; // Revenez à l'état initial
+      document.getElementById("balrog").style.display="block"; // Fait apparaitre le balrog
+      etat = "visible"; // Reviens à l'état initial
     }
   });
